c84998938b06657314e1ac62f7a0a991c6e9a55f
Joon Kang <joonkang@monzo.com>
2024-01-07 19:57:10 +0000

    feat: shutdown

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -24,12 +24,12 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 		time.Sleep(time.Minute)
 
 		mu.Lock()
+		defer mu.Unlock()
 		for ip, client := range clients {
 			if time.Since(client.lastSeen) > 3*time.Minute {
 				delete(clients, ip)
 			}
 		}
-		mu.Unlock()
 	}()
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if app.config.limiter.enabled {
@@ -40,17 +40,15 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 			}
 
 			mu.Lock()
+			defer mu.Unlock()
 			if _, found := clients[ip]; !found {
 				clients[ip] = &client{limiter: rate.NewLimiter(rate.Limit(app.config.limiter.rps), app.config.limiter.burst)}
 			}
 			clients[ip].lastSeen = time.Now()
-
 			if !clients[ip].limiter.Allow() {
-				mu.Unlock()
 				app.rateLimitExceededResponse(w, r)
 				return
 			}
-			mu.Unlock()
 		}
 		next.ServeHTTP(w, r)
 	})
